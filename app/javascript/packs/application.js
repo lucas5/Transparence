a9b77a8459af1a2768e892bb91b8f5a2ff882a27
Lucas Expedito <lucasexp2013@gmail.com>
2021-09-16 23:55:18 -0300

    Remove unused dependencies (2)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,20 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import 'bootstrap';
-
-document.addEventListener("turbolinks:load", () => {
-    $('[data-toggle="tooltip"]').tooltip()
-});
-
-import '../stylesheets/application'; // This file will contain your custom CSS
+// This file will contain your custom CSS
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-require('admin-lte');
-
-import "@fortawesome/fontawesome-free/js/all";
